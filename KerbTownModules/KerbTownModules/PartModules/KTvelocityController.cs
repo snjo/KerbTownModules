e034cc4cbc443c2d4c3c9218329af8cad248c06c
Andreas Aakvik Gogstad <agogstad@gmail.com>
2013-09-22 15:56:57 +0200

    velocity controller particles and resource use

--- KerbTownModules/KerbTownModules/PartModules/KTvelocityController.cs
+++ KerbTownModules/KerbTownModules/PartModules/KTvelocityController.cs
@@ -12,31 +12,76 @@ class KTvelocityController : PartModule
     [KSPField]
     public float maxThrust = 2f;
     [KSPField]
-    public float resourceConsumption = 1f;
+    public float resourceConsumption = 0.1f;
+    [KSPField]
+    public string resourceName = "MonoPropellant";
     [KSPField]
     public float lowerSpeedThreshold = 0.1f;
     [KSPField]
     public float upperSpeedThreshold = 2.0f;
+    [KSPField]
+    public bool useFX = true;
+    [KSPField]
+    public string particleTextureName = string.Empty;
+    [KSPField]
+    public Vector3 EmitterLocalVelocity = new Vector3(0f, 0f, 1f);
+    [KSPField]
+    public string transformThrustDirection = "forward";
+    [KSPField]
+    public string thrustKey = "delete";
+    [KSPField]
+    public float minVelocityToActivate = 0.0f;
 
     public bool controllerActive = false;
 
     Transform[] transformArray;
     private bool transformsFound = false;
     private Vector3 velocityDirection = new Vector3(0f, 0f, 0f);
+    private KerbTownModules.KTparticleFX[] particleFX;
+    private Texture2D particleTexture;
+
+    private float defaultEmitterMinEmission = 120f;
+    private float defaultEmitterMaxEmission = 160f;
 
     public override void OnStart(PartModule.StartState state)
     {
         base.OnStart(state);
+        if (!HighLogic.LoadedSceneIsFlight) return;
+
         transformArray = part.FindModelTransforms(thrustTransformName);
         if (transformArray.Length > 0)
         {
-
             transformsFound = true;
+            particleFX = new KerbTownModules.KTparticleFX[transformArray.Length];
         }
         else
         {
             Debug.Log("KTvelocityController: Transforms not found named " + thrustTransformName + ", disabling the module");
             this.enabled = false;
+        }
+
+        if (this.enabled && particleTextureName != string.Empty)
+        {
+            // set up fx ---- TODO, assign each transform its own FX
+            particleTexture = GameDatabase.Instance.GetTexture(particleTextureName, false);
+            if (particleTexture != null)
+            {
+                for (int i = 0; i < particleFX.Length; i++)
+                {
+                    particleFX[i] = new KerbTownModules.KTparticleFX(transformArray[i].gameObject, particleTexture);
+                    particleFX[i].EmitterLocalVelocity = EmitterLocalVelocity;
+                    //Debug.Log("KTvelocityController: particle texture found: " + particleTextureName);
+                    particleFX[i].setupFXValues();
+                    particleFX[i].pEmitter.minEmission = 0f;
+                    particleFX[i].pEmitter.maxEmission = 0f;
+                    particleFX[i].pEmitter.useWorldSpace = false;
+                }
+            }
+            else
+            {
+                useFX = false;
+                Debug.Log("KTvelocityController: particle texture not found, disabling fx");
+            }
         }
     }
 
@@ -55,31 +100,55 @@ class KTvelocityController : PartModule
                 //Debug.Log("failed to find rigidbody velocity");
                 return;
             }
+
             int i = 0;
-            //Debug.Log("Entering loop");
+                        
+            bool doThrust = Input.GetKey(thrustKey);
+
             foreach (Transform t in transformArray)
-            {
+            {                                
+                float thrustUsed = updateThruster(i, doThrust, t);
+                consumeResource(thrustUsed * maxThrust);
                 i++;
-                //Debug.Log("loop: " + i);
-                float thrustModifier = Vector3.Dot(t.transform.forward, velocityDirection.normalized);
-                if (thrustModifier < 0f)
-                {
-                    //Debug.Log("thrust mod:  " + thrustModifier);
-                    if (Input.GetKey(KeyCode.Space))
-                    {
-                        try
-                        {
-                            part.gameObject.rigidbody.AddForceAtPosition(t.transform.forward * -thrustModifier * maxThrust, t.transform.position);
-                        }
-                        catch
-                        {
-                            Debug.Log("Error adding force");
-                        }
-                        //Debug.Log("force: " + thrustModifier + " at " + i);
-                    }
-                }
             }
         }
+    }
+
+    private void consumeResource(float modifier)
+    {
+        if (modifier > 0f && !CheatOptions.InfiniteRCS)
+        {
+            base.part.RequestResource(resourceName, resourceConsumption * modifier * Time.deltaTime);
+        }
+    }
+
+    private float updateThruster(int fxNumber, bool doThrust, Transform t)
+    {
+        Vector3 thrustDirection;
+        float thrustModifier = 0f;
+        if (doThrust)
+        {
+            if (transformThrustDirection == "up")
+                thrustDirection = t.transform.up;
+            else
+                thrustDirection = t.transform.forward;
+
+            thrustModifier = Vector3.Dot(thrustDirection, velocityDirection.normalized);
+            if (thrustModifier > 0f && velocityDirection.magnitude > minVelocityToActivate)
+            {
+                part.gameObject.rigidbody.AddForceAtPosition(-thrustDirection * thrustModifier * maxThrust, t.transform.position);
+            }
+            else
+            {
+                thrustModifier = 0f;
+            }
+        }
+        if (useFX)
+        {
+            particleFX[fxNumber].pEmitter.minEmission = defaultEmitterMinEmission * thrustModifier;
+            particleFX[fxNumber].pEmitter.maxEmission = defaultEmitterMaxEmission * thrustModifier;
+        }
+        return thrustModifier;
     }
 }
 
